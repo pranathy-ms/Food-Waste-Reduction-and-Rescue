98d67b40f0503f0aa5b196211e5c73d4e5991f2b
Srivasa Pranathy Mutcherla <smutcherla@hawk.iit.edu>
2024-11-28 20:53:17 -0700

    modified:   data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -20,10 +20,51 @@ solutions_df = solutions_df.groupby(['solution_group',
                          'solution_priority_action_area', 
                          'solution_name', 
                          'sector', 
-                         'state', 
+                         #'state', 
                          'food_type']).agg(aggregation_functions)
 
 
 solutions_df.reset_index(inplace=True)
 
 solutions_df.to_csv('food_waste_solutions_processed.csv', index=False)
+
+surplus_df = pd.read_csv('ReFED_US_Food_Surplus_Detail.csv', skiprows=1, low_memory=False)
+surplus_df = surplus_df[surplus_df['sector'] == 'Retail']
+surplus_df = surplus_df.drop(columns=['sub_sector', 'sub_sector_category', 'food_category'])
+
+aggregation_functions = {
+    'tons_surplus': 'sum',
+    'tons_supply': 'sum',
+    'us_dollars_surplus': 'sum',
+    'tons_waste': 'sum',
+    'tons_uneaten': 'sum',
+    'tons_inedible_parts': 'sum',
+    'tons_not_fit_for_human_consumption': 'sum',
+    'tons_donations': 'sum',
+    'tons_industrial_uses': 'sum',
+    'tons_animal_feed': 'sum',
+    'tons_anaerobic_digestion': 'sum',
+    'tons_composting': 'sum',
+    'tons_not_harvested': 'sum',
+    'tons_incineration': 'sum',
+    'tons_land_application': 'sum',
+    'tons_landfill': 'sum',
+    'tons_sewer': 'sum',
+    'tons_dumping': 'sum',
+    'surplus_upstream_100_year_mtco2e_footprint': 'sum',
+    'surplus_downstream_100_year_mtco2e_footprint': 'sum',
+    'surplus_total_100_year_mtco2e_footprint': 'sum',
+    'surplus_upstream_100_year_mtch4_footprint': 'sum',
+    'surplus_downstream_100_year_mtch4_footprint': 'sum',
+    'surplus_total_100_year_mtch4_footprint': 'sum',
+    'gallons_water_footprint': 'sum',
+    'meals_wasted': 'sum'
+}
+
+surplus_df = surplus_df.groupby(['year', 
+                         'sector', 
+                         'food_type']).agg(aggregation_functions)
+
+# Reset index to convert grouped keys into columns
+surplus_df = surplus_df.reset_index()
+surplus_df.to_csv('food_waste_surplus_processed.csv', index=False)
